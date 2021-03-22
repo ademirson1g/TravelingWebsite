1fd527bb461534392576a868a88f72be49867c87
ademirson <ademirson15@hotmail.com>
2021-03-22 20:03:52 +0100

    config

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+class Config {
+  const DB_Host = "";
+  const DB_USERNAME = "";
+  const DB_PASSWORD = "";
+  const DB_SCHEME = "mysql";
+  
+
+
+}
+define("MYSQLHOST", "localhost");
+
+ ?>
