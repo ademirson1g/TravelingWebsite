795b77bf9e3b39c23e922a6e7b7627e8262c80e4
ademirson <ademirson15@hotmail.com>
2021-04-06 20:57:11 +0200

    .

--- api/dao/Account.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-class AccountDao extends BaseDao{
-  public function add_account($account){}
-}
- ?>
--- api/dao/User.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class UserDao extends BaseDao{
-  public function get_user_by_email($email){
-  
-  }
-}
- ?>
