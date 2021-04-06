b8b8f23c4098d7b1d225396d0396057c8aa954b2
ademirson <ademirson15@hotmail.com>
2021-04-06 17:57:15 +0200

    sdadsa

--- api/config.php
+++ api/config.php
@@ -1,13 +1,12 @@
 <?php
 class Config {
-  const DB_Host = "";
-  const DB_USERNAME = "";
-  const DB_PASSWORD = "";
+  const DB_Host = "localhost";
+  const DB_USERNAME = "root";
+  const DB_PASSWORD = "ademir299";
   const DB_SCHEME = "mysql";
-  
+
 
 
 }
-define("MYSQLHOST", "localhost");
 
  ?>
