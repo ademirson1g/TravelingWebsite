83f5fe05d63830993187da5e16bbffd0faa0af6e
ademirson <ademirson15@hotmail.com>
2021-04-07 03:00:31 +0200

    .

--- index.php
+++ index.php
@@ -36,18 +36,18 @@
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=flights"></span>Flight List</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=about">About</a></li>
-                        
-                     
+
+
                     </ul>
                 </div>
             </div>
         </nav>
-       
-        <?php 
+
+        <?php
         $page = isset($_GET['page']) ?$_GET['page'] : "home";
         include $page.'.php';
         ?>
-       
+
 
 <div class="modal fade" id="confirm_modal" role='dialog'>
     <div class="modal-dialog modal-md" role="document">
@@ -73,53 +73,7 @@
       </div>
       <div class="modal-body">
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-righ t"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      </div>
-    </div>
-  </div>
-  <div id="preloader"></div>
-        <footer class="bg-light py-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="mt-0">Contact us</h2>
-                        <hr class="divider my-4" />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-                        <div><?php echo $_SESSION['setting_contact'] ?></div>
-                    </div>
-                    <div class="col-lg-4 mr-auto text-center">
-                        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
-                        <a class="d-block" href="mailto:<?php echo $_SESSION['setting_email'] ?>"><?php echo $_SESSION['setting_email'] ?></a>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="container"><div class="small text-center text-muted"> <?php echo $_SESSION['setting_name'] ?> | <a href="https://www.campcodes.com" target="_blank">CampCodes</a></div></div>
-        </footer>
-        
-       <?php include('footer.php') ?>
+
     </body>
 
     <?php $conn->close() ?>
--- signup.php
+++ signup.php
@@ -30,29 +30,3 @@
 		display:none;
 	}
 </style>
-<script>
-	$('#signup-frm').submit(function(e){
-		e.preventDefault()
-		$('#signup-frm button[type="submit"]').attr('disabled',true).html('Saving...');
-		if($(this).find('.alert-danger').length > 0 )
-			$(this).find('.alert-danger').remove();
-		$.ajax({
-			url:'admin/ajax.php?action=signup',
-			method:'POST',
-			data:$(this).serialize(),
-			error:err=>{
-				console.log(err)
-		$('#signup-frm button[type="submit"]').removeAttr('disabled').html('Create');
-
-			},
-			success:function(resp){
-				if(resp == 1){
-					location.reload();
-				}else{
-					$('#signup-frm').prepend('<div class="alert alert-danger">Email already exist.</div>')
-					$('#signup-frm button[type="submit"]').removeAttr('disabled').html('Create');
-				}
-			}
-		})
-	})
-</script>
